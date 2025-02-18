30a703e8702b0a961ca264e984e326bb6df64ab2
operlaston <operlaston21@gmail.com>
2025-02-18 18:59:18 -0500

    Added two links at the top of the page (placeholder)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 
 function App() {
   return (
     <Router>
+      <Link to="/">Home</Link>
+      <Link to="/login">Login</Link>
       <Routes>
         {/* Route for the home page */}
         <Route path="/" element={<Home />} />
