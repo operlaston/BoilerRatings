3e910bf973e04291158b1d7c517d0b2249435263
Bryce Miller <brycerellim@gmail.com>
2025-02-22 21:14:59 -0500

    Rudimentory email 2fa working

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
+const authController = require("./controllers/authoController")
 
 // connect to database
 mongoose.connect(process.env.MONGODB_URI)
@@ -17,5 +18,12 @@ mongoose.connect(process.env.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 
+const PORT = 5000; //We can change this in the .env file later if we want a differnt port just put this for testing
+app.listen(PORT, () => {
+  console.log(`Running on http://localhost:${PORT}`)
+})
+
+app.post("/api/auth/signup", authController.signup)
+app.post("/api/auth/verify-code", authController.verifyCode)
 
 module.exports = app
