384c45eb25525192c6e198a2df086f18546dbeee
Bryce Miller <brycerellim@gmail.com>
2025-03-01 16:11:34 -0500

    fixed to work with updated review schema

--- backend/controllers/review.js
+++ backend/controllers/review.js
@@ -5,6 +5,7 @@ reviewRouter.get('/:id', async (req, res) => {
     try {
         const review = await Review.findById(req.params.id)
         .populate('instrutor', 'instructorName gpa')
+        .populate('user', 'username email')
         .populate('reports')
         if (!review) {
             return res.status(401).json({error : 'Review not found'})
