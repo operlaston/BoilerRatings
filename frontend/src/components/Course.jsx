f5edb30272b5c7b28cb85839b72daf7a83ff32df
operlaston <operlaston21@gmail.com>
2025-03-01 15:58:09 -0600

    made search bar bigger and added more vertical padding to each course card

--- frontend/src/components/Course.jsx
+++ frontend/src/components/Course.jsx
@@ -1,6 +1,6 @@
 const Course = ({name, number, credits, enjoyment, difficulty, recommended, numReviews, requirements}) => {
   return (
-    <div className="bg-gray-800 text-white py-3 px-5 rounded-lg cursor-pointer
+    <div className="bg-gray-800 text-white py-4 px-5 rounded-lg cursor-pointer
       hover:scale-102 transition-all
     ">
       <div
