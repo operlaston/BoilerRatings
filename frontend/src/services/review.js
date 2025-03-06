583f7468f0c7d7b716c1d80de0a2761871cdf272
Bryce Miller <brycerellim@gmail.com>
2025-03-06 18:18:48 -0500

    corrected route

--- frontend/src/services/review.js
+++ frontend/src/services/review.js
@@ -3,12 +3,12 @@ import axios from "axios"
 const baseurl = 'http://localhost:3000'
 
 const getReviewsForACourse = async (course) => {
-    const response = await axios.get(`${baseurl}/reviews/course`, course)
+    const response = await axios.get(`${baseurl}/api/reviews/course`, course)
     return response.data
 }
 
 const addReview = async ( review, course ) => {
-    const response = await axios.post(`${baseurl}/reviews`, {review, course})
+    const response = await axios.post(`${baseurl}/api/reviews`, {review, course})
     return response.data()
 }
 //I don't know how you have a review id without the rest of the data but just incase
