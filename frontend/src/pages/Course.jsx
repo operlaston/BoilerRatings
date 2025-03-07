3ad6d23ffd36fcab1ef3e1ae7c0ede71cc1e4f0b
operlaston <operlaston21@gmail.com>
2025-03-06 22:07:43 -0500

    added back arrow navigation from course to home page

--- frontend/src/pages/Course.jsx
+++ frontend/src/pages/Course.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import CoursePanel from "../components/CoursePanel";
 import { ArrowLeft } from "lucide-react";
 import ReviewPage from "./ReviewPage";
+import { useNavigate } from "react-router-dom";
 
 function CourseInfo() {
+  const navigate = useNavigate();
+
   return (
     <div className="w-full h-min-screen flex items-center justify-center p-4 dark:bg-gray-900 overflow-y-auto">
       <div className="absolute top-2/3 w-40 h-40 bg-gray-200 dark:bg-gray-700 rounded-full mix-blend-multiply dark:mix-blend-normal filter blur-2xl opacity-70 animate-blob-1 ease-in-out duration-150  translate-x-2/3 -translate-y-1/4"></div>
@@ -11,7 +14,7 @@ function CourseInfo() {
       <div className="absolute top-1/2 w-52 h-52 bg-gray-200 dark:bg-gray-700 rounded-full mix-blend-multiply dark:mix-blend-normal filter blur-2xl opacity-70 animate-blob-3 ease-in-out duration-200 -translate-x-2/3"></div>
       <div className="relative flex flex-col w-full max-w-7xl min-h-screen py-12">
         <button className="absolute text-white -left-15 top-20 rounded-full cursor-pointer items-center ">
-          <ArrowLeft className="text-white h-10 w-10" />
+          <ArrowLeft className="text-white h-10 w-10 cursor-pointer" onClick={() => navigate('/')} />
         </button>
         <CoursePanel />
         <ReviewPage />
