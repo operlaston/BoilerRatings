3697794f839a735d57bc802bf4307d7143470ad5
operlaston <operlaston21@gmail.com>
2025-03-15 13:27:06 -0500

    refactored backend major endpoints to use awa-to-js to() function for cleaner error handling

--- backend/controllers/major.controller.js
+++ backend/controllers/major.controller.js
@@ -1,24 +1,37 @@
 const majorsRouter = require('express').Router()
+const {to} = require('await-to-js')
 const Major = require('../models/major')
 
 majorsRouter.get('/', async (req, res) => {
-  try {
-    const majors = await Major.find({})
-    res.status(200).json(majors)
+  const [error, majors] = await to(Major.find({}))
+  if (error) {
+    console.error(error);
+    return res.status(500).json({error: "server error"})
   }
-  catch (err) {
-    res.status(400).json({error: "bad request"})
-  }
+  return res.status(200).json(majors)
+  // try {
+  //   const majors = await Major.find({})
+  //   res.status(200).json(majors)
+  // }
+  // catch (err) {
+  //   res.status(400).json({error: "bad request"})
+  // }
 })
 
 majorsRouter.get('/:id', async (req, res) => {
-  try {
-    const major = await Major.findById(req.params.id)
-    res.status(200).json(major)
+  const [error, major] = await to(Major.findById(req.params.id))
+  if (error) {
+    console.error(error)
+    return res.status(404).json({error: "id doesn't exist"})
   }
-  catch (err) {
-    res.status(404).json({error: "id doesn't exist"})
-  }
+  res.status(200).json(major)
+  // try {
+  //   const major = await Major.findById(req.params.id)
+  //   res.status(200).json(major)
+  // }
+  // catch (err) {
+  //   res.status(404).json({error: "id doesn't exist"})
+  // }
 })
 
 majorsRouter.post('/', async (req, res) => {
