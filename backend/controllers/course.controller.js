93262d906009c2581552bf2f637ae3ef1af28477
gpuls <gavin.puls@icloud.com>
2025-03-16 16:42:44 -0400

    added backend for getting courses matching requirement

--- backend/controllers/course.controller.js
+++ backend/controllers/course.controller.js
@@ -2,6 +2,26 @@ const courseRouter = require('express').Router()
 const { populate } = require('../models/review')
 const Course = require('../models/course')
 
+// get courses that match requirement
+courseRouter.get('/requirement', async (req, res) => {
+    const { requirement } = req.body
+    try {
+        if (requirement == null) {
+            return res.status(401).json({ "error": "null requirement string" })
+        }
+
+        const courses = await Course.find({ requirements: requirement })
+        if (courses.length == 0) {
+            return res.status(404).json({ "error": "no courses found" })
+        }
+
+        return res.status(200).json(courses)
+    }
+    catch (err) {
+        return res.status(400).json({ "error": "bad request" })
+    }
+})
+
 courseRouter.get('/:id', async (req, res) => {
     try {
         const course = await Course.findById(req.params.id)
@@ -10,13 +30,13 @@ courseRouter.get('/:id', async (req, res) => {
         })
         .populate('prerequisites', 'name number')
         .populate('instructors', 'name')
-        if (!course) {
+        if (course) {
             return res.status(401).json({error: 'Course not found'})
         } 
         res.status(200).json(course)
     } catch (err) {
         console.log('Course Fetch error', err)
-        res.status(400).json({error: "bad request"})
+        res.status(400).json({error: "1 bad request"})
     }
 })
 
@@ -31,7 +51,7 @@ courseRouter.get('/', async (req, res) => {
         res.status(200).json(courses)
     } catch (error) {
         console.log("Error fetching courses", error)
-        res.status(400).json({error: "bad request"})
+        res.status(400).json({error: "2 bad request"})
     }
 })
 
