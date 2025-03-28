9de42545a8b6bf24bd7c24c914bd3bd4b5a35962
operlaston <operlaston21@gmail.com>
2025-03-27 20:01:51 -0400

    created requirement schema and requirement controller file

--- backend/models/requirement.js
+++ backend/models/requirement.js
@@ -1,7 +1,19 @@
 const mongoose = require('mongoose')
 
-// TBD
 const requirementSchema = new mongoose.Schema({
   name: String,
-
+  courses: [{type: [{type: String}]}],
 })
+
+requirementSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    if (!returnedObject._id) return
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+const Requirement = mongoose.Model('Requirement', requirementSchema)
+
+module.exports = Requirement
