2863fb7f29f1a61d077bdf6dc1e2aa5d412226d8
Bryce Miller <brycerellim@gmail.com>
2025-04-23 14:12:31 -0400

    page reports routers

--- frontend/src/services/pagereport.service.js
+++ frontend/src/services/pagereport.service.js
@@ -4,6 +4,17 @@ const baseurl = 'http://localhost:3000'
 
 const sendReport = async (page, reportContent) => {
     const response = await axios.post(`${baseurl}/api/pagereports`, {page, reportContent})
+    return response.data
 }
 
-export { sendReport }
+const getReports = async () => {
+    const response = await axios.get(`${baseurl}/api/pagereports/`)
+    return response.data
+}
+
+const deleteReport = async(id) => {
+    const response = await axios.delete(`${baseurl}/api/pagereports/${id}`)
+    return response.data
+}
+
+export { sendReport, getReports, deleteReport }
