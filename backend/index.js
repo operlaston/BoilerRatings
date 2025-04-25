b2e47b33de05ee7a2e2f40c462573fb3e08fc890
operlaston <operlaston21@gmail.com>
2025-04-25 18:58:03 -0500

    fix

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,13 @@ const app = require('./app')
 const cron = require('node-cron');
 const axios = require('axios');
 
+app.listen(config.PORT, () => {
+  console.log('server has started')
+})
+
 //5 second testing string */5 * * * * *
 //Normal string 1 0 * * *
-cron.schedule('*/5 * * * * *', async () => {
+cron.schedule('1 0 * * *', async () => {
   try {
     console.log('Running inactive user email task at 12:01 AM');
     await axios.get('http://localhost:3000/api/users/inactive'); // or your full production URL
@@ -13,8 +17,3 @@ cron.schedule('*/5 * * * * *', async () => {
     console.error('Failed to trigger inactive user route:', err.message);
   }
 });
-
-
-app.listen(config.PORT, () => {
-  console.log('server has started')
-})
